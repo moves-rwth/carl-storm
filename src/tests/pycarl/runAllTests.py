7dd32029192f94a698595c9bd9feec552fd4ba98
Sebastian Junges <sebastian.junges@ru.nl>
2023-03-27 17:26:45 +0200

    remove some irrelevant tests

--- src/tests/pycarl/runAllTests.py
+++
@@ -1,9 +0,0 @@
-import os
-import unittest
-
-basedir = os.path.dirname(os.path.realpath(__file__))
-loader = unittest.TestLoader()
-tests = loader.discover(basedir, pattern = "Test_*.py")
-
-runner = unittest.TextTestRunner()
-runner.run(tests)
