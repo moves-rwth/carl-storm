f0e78058951222a76a2fea4ce8743c23fdaf28a5
Matthias Volk <m.volk@utwente.nl>
2023-07-31 14:29:23 +0200

    Removed pycarl bindings

--- resources/addons/addons.cmake
+++ resources/addons/addons.cmake
@@ -2,17 +2,12 @@ include(CMakeDependentOption)
 
 option(BUILD_ADDONS "Build addons" OFF)
 cmake_dependent_option(BUILD_ADDON_PARSER "Build parser addon" OFF "BUILD_ADDONS" OFF)
-cmake_dependent_option(BUILD_ADDON_PYCARL "Build python binding addon" OFF "BUILD_ADDONS" OFF)
 
 if(BUILD_ADDONS)
 
 	add_custom_target(addons)
 	add_dependencies(addons lib_carl)
 	set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/addons)
-	
-	if(BUILD_ADDON_PYCARL)
-		set(BUILD_ADDON_PARSER ON)
-	endif()
 
 	if(BUILD_ADDON_PARSER)
 		if(NOT USE_GINAC)
@@ -20,8 +15,4 @@ if(BUILD_ADDONS)
 		endif()
 		include(resources/addons/carl-parser.cmake)
 	endif()
-	if(BUILD_ADDON_PYCARL)
-		include(resources/addons/pycarl.cmake)
-	endif()
-
 endif()
