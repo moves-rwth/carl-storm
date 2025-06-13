5c8b3310725e40ecd9c048a0978c61ab7833cf7b
Matthias Volk <m.volk@tue.nl>
2025-06-13 14:42:58 +0200

    Bump googletest to 1.12

--- resources/gtest.cmake
+++ resources/gtest.cmake
@@ -7,23 +7,16 @@ ExternalProject_Add(
 	BUILD_COMMAND cmake --build . --config ${CMAKE_BUILD_TYPE} --target gtest
 	COMMAND cmake --build . --config ${CMAKE_BUILD_TYPE} --target gtest_main
 	INSTALL_COMMAND ""
-	BUILD_BYPRODUCTS ${PROJECT_BINARY_DIR}/resources/src/GTest-EP-build/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT} ${PROJECT_BINARY_DIR}/resources/src/GTest-EP-build/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}
+	BUILD_BYPRODUCTS ${PROJECT_BINARY_DIR}/resources/src/GTest-EP-build/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT} ${PROJECT_BINARY_DIR}/resources/src/GTest-EP-build/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}
 )
 
 ExternalProject_Get_Property(GTest-EP source_dir)
 ExternalProject_Get_Property(GTest-EP binary_dir)
 
-if(WIN32)
-	add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/${CMAKE_BUILD_TYPE}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
-	add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/${CMAKE_BUILD_TYPE}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
+add_imported_library(GTESTCORE STATIC "${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
+add_imported_library(GTESTMAIN STATIC "${binary_dir}/lib/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
 
-	set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC)
-else()
-	add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
-	add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
-
-	set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
-endif()
+set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
 
 add_dependencies(GTESTCORE_STATIC GTest-EP)
 add_dependencies(GTESTMAIN_STATIC GTest-EP)
