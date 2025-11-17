a04e5041afbf42776b05d3ea0b88f0406cafa342
Matthias Volk <volkm@users.noreply.github.com>
2025-11-17 16:39:26 +0100

    Use newer googletest version (#101)
    Fix for issues with Clang 21

--- resources/gtest.cmake
+++ resources/gtest.cmake
@@ -1,7 +1,10 @@
 include(FetchContent)
 FetchContent_Declare(
 	googletest
-	URL https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.zip
+	# Use commit with workaround for char conversion warning, being recognized by Clang 21+.
+	# See https://github.com/google/googletest/issues/4762
+	URL https://github.com/google/googletest/archive/${GTEST_VERSION}.zip
+	#URL https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.zip
 )
 FetchContent_MakeAvailable(googletest)
 
