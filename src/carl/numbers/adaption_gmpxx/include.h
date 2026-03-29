1e8a90f7ea46d8b5f737cf478cf9475ae88b39cb
Matthias Volk <m.volk@tue.nl>
2026-03-29 21:58:05 +0200

    Ignore compiler warning in gmpxx

--- src/carl/numbers/adaption_gmpxx/include.h
+++ src/carl/numbers/adaption_gmpxx/include.h
@@ -5,4 +5,19 @@ static_assert(false, "This file may only be included indirectly by numbers.h");
 #endif
 
 #include <cstddef>
+
+// Disable potential warning on newer AppleClang versions
+#if __GNUC__ && defined( __has_warning )
+#if __has_warning("-Wdeprecated-literal-operator")
+#define SUPPRESSING
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-literal-operator"
+#endif
+#endif
+
 #include <gmpxx.h>
+
+#ifdef SUPPRESSING
+#undef SUPPRESSING
+#pragma GCC diagnostic pop
+#endif
